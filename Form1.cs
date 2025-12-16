84b4a48259e4c778127e5ed084bd2138abe6629e
Frayers67 <adrians.zibeni@gmail.com>
2025-12-16 11:39:43 +0100

    Add HTML layout for Form1 with navigation options

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="lv">
+<head>
+    <meta charset="UTF-8">
+    <title>1. Forma: Sākums</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container welcome-container">
+        <div class="sidebar blue-bg">
+            <h1>Esi sveicināts ĶLUS</h1>
+            <p>Tavas laboratorijas uzskaites sistēma</p>
+            </div>
+
+        <div class="content white-bg">
+            <h2>Ko Tu vēlies darīt šodien?</h2>
+            <p class="description-text">Piekļūsti savas skolas laboratorijai vai aplūko publiski pieejamo informāciju un datus par laboratoriju.</p>
+            
+            <div class="button-group">
+                <a href="forma2.html" class="button primary">Pieslēgties</a>
+                
+                <a href="forma3.html" class="button secondary">Piekļūt publiskiem datiem</a>
+            </div>
+        </div>
+    </div>
+    <div class="error-report">
+        <button class="error-button">Ziņot par kļūdu</button>
+    </div>
+</body>
+</html>
